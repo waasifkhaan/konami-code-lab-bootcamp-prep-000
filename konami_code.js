cf7f770ea29cc29c929ab13cd476fef82bf03c64
Wasif Khan <waasif.khaan@gmail.com>
2018-04-17 20:52:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-  const bdy = document.getElementByTag('body')
+  const bdy = document.querySelector('body')
 bdy.addEventListener('keydown', function(e){
     const key = parseInt(e.detail || e.which);
 
