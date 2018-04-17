19cafea8fc7c440af4fd01820e5a4dbf1f5e9c98
Wasif Khan <waasif.khaan@gmail.com>
2018-04-17 20:51:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,10 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
+  const bdy = document.getElementByTag('body')
+bdy.addEventListener('keydown', function(e){
+    const key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
     index++;
@@ -18,7 +17,7 @@ function onKeyDownHandler(e) {
   } else {
     index = 0;
   }
-}
+})
   // Write your JavaScript code inside the init() function
 
 }
